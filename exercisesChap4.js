29ecd6f8ce74b5304ceba24741ab3d8e59f6347d
abeer <aabeer_alb@icloud.com>
2017-12-16 20:17:42 +0200

    Day 3

---
+++ exercisesChap4.js
@@ -0,0 +1,48 @@
+// sum of a range.
+// console.log(sum(range(1, 10)));
+function range(start,end) {
+  var rangeArr = [];
+  for (var i = start; i <= end; i++) {
+    rangeArr.push(i);
+  }
+  return rangeArr;
+}
+
+function sum(array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+
+console.log(sum(range(1,10))); //55
+
+// Reversing an array.
+function reverseArray(array) {
+  var finalArray = [];
+  for (var i = array.length -1; i >= 0; i--) {
+    finalArray.push(array[i]);
+  }
+  return finalArray;
+}
+
+function reverseInPlace(array) {
+  for (var i = 0; i < Math.floor(array.length/2); i++) {
+    var temp = array[i];
+    array[i] = array[array.length-1-i];
+    array[array.length-1-i] = temp;
+  }
+  return array;
+}
+
+// Lists
+function arrayToList(array) {
+  var list = {};
+  for (var i = 0; i < array.length; i++) {
+    list.value = array[i];
+    list.rest = list;
+  }
+  return list;
+}
+console.log(arrayToList([1,2,3]));
