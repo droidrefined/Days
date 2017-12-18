4006038f8bd739c6dd8de0aba0e0bc29b02afa19
abeer <aabeer_alb@icloud.com>
2017-12-18 16:58:35 +0200

    Day 4

--- exercisesChap4.js
+++ exercisesChap4.js
@@ -36,17 +36,71 @@ function reverseInPlace(array) {
   return array;
 }
 
-// Lists
+// Lists Exercise
 function arrayToList(array) {
   var list = {value: array[array.length-1], rest:null};
-  for (var i = array.length-2; i >= 0; i--) {
+  for (var i = array.length-1-1; i >= 0; i--) {
     var temp = list;
     list = {value: array[i], rest:temp};
   }
   return list;
 }
-console.log(arrayToList([1,2,3,4,5]));
+// console.log(arrayToList([1,2,3,4,5]));
 
-function listToArray() {
-
+function listToArray(list) {
+  var list = list;
+  var arr = [];
+  for (var node = list; node; node = node.rest) {
+    arr.push(node.value);
+  }
+  return arr;
 }
+
+// console.log(listToArray(arrayToList([1,2,3,4,5])));
+
+function prepend(element,list) {
+  var newList = { value:element, rest:list }
+  return newList;
+}
+
+// console.log(prepend(10,list));
+
+function nth(list,nthNumber) {
+  var arr = [];
+  for (var node = list; node; node = node.rest) {
+    arr.push(node.value);
+  }
+  return arr[nthNumber];
+}
+// var list = arrayToList([1,2,3,4,5]);
+// console.log(nth(list,1));
+
+function deepEqual(obj1,obj2) {
+  var temp1 = [], temp2 = [];
+  for (var i in obj1) {
+    temp1.push(i);
+  }
+  for (var i in obj2) {
+    temp2.push(i);
+  }
+  if (temp1.length == temp2.length) {
+    var count = 0;
+    for (var i = 0; i < temp1.length; i++) {
+      if ((((typeof obj1[temp1[i]]) === (typeof obj2[temp2[i]])) && ((obj1[temp1[i]] == obj2[temp2[i]])))) {
+        count += 1;
+      }
+    }
+  }
+  if (count == (temp1.length -1)) {
+    return true;
+  }
+}
+// var objz = {a:{},b:{},c:3}
+// var obarr=[];
+// for (var i in objz) {
+//   obarr.push(i);
+// }
+// console.log(obarr);
+// console.log(typeof (objz[obarr[0]]));
+//
+// console.log(objz[obarr[0]]);
